b4d45f3c834bfc40c1f95cccf81df9ba5dedbd81
manojnpalat <manojnp@gmail.com>
2025-07-12 11:44:48 +0530

    bouncy

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
-# Exercise 1.5
+# Exercise 1.5 The Bouncing Ball
+
+height = 100
+for i in range(10):
+	nextHeight = round(height*3/5, 4)
+	height = nextHeight
+	print(height)
+
+		
